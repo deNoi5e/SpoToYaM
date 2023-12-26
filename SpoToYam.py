4358810ba57c900ea677d7b5778fb304b7eb08a9
Anton Rusty <rusty@deeplay.io>
2023-12-26 13:26:50 +0600

    Рефакторинг

--- SpoToYam.py
+++ SpoToYam.py
@@ -13,46 +13,106 @@ from config_reader import config
 # Logging
 logging.basicConfig(level=logging.INFO)
 
-# Создайте экземпляр класса Client для аутентификации в яндекс
-client = Client(config.client_token_yandex.get_secret_value()).init()
 
-# Создадим плейлист с названием + текущая дата
-user_playlists = client.users_playlists_list(client.me.account.uid)
+def main():
+    # Создайте экземпляр класса Client для аутентификации в яндекс
+    client = Client(config.client_token_yandex.get_secret_value()).init()
 
-# Название генерируемого плейлиста
-playlist_name = "Received from Spotify"
+    # Создадим плейлист с названием + текущая дата
+    user_playlists = client.users_playlists_list(client.me.account.uid)
 
-# Класс для создания запросов к плейлисту
-playlist_difference = Difference()
+    # Название генерируемого плейлиста
+    playlist_name = "Received from Spotify"
 
-# Инициализация плейлиста для работы
-edited_playlist = ''
+    # Класс для создания запросов к плейлисту
+    playlist_difference = Difference()
 
-# Аутентификация через Spotify API
-auth_manager = SpotifyClientCredentials(
-    client_id=config.client_id_spotify.get_secret_value(),
-    client_secret=config.client_token_spotify.get_secret_value())
-sp = spotipy.Spotify(auth_manager=auth_manager)
+    # Инициализация плейлиста для работы
+    global edited_playlist
 
-# Получаем плейлист из Spotify
-playlist = sp.playlist(config.spotify_playlist_id)
+    # Аутентификация через Spotify API
+    auth_manager = SpotifyClientCredentials(
+        client_id=config.client_id_spotify.get_secret_value(),
+        client_secret=config.client_token_spotify.get_secret_value())
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# Создаем CSV-файл для записи данных
-with open("exported.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    writer.writerow(["Artist", "Track"])  # Записываем заголовки столбцов
+    # Получаем плейлист из Spotify
+    target_playlist_from_spotify = sp.playlist(config.spotify_playlist_id)
 
-    # Перебираем треки в плейлисте и записываем информацию в CSV-файл
-    for track in playlist["tracks"]["items"]:
-        artist = track["track"]["artists"][0]["name"]
-        track_name = track["track"]["name"]
-        writer.writerow([artist, track_name])
+    generate_csv_playlist(target_playlist_from_spotify)
+
+    # Проверим наличие нужного плейлиста и создадим при необходимости
+    if check_created_playlist(user_playlists, playlist_name):
+        client.users_playlists_create(title=playlist_name, visibility='private', user_id=client.me.account.uid)
+        logging.info("Плейлист создан")
+    else:
+        logging.info("Не нужно создавать плейлист")
+
+    # Очистим плейлист, перед работой с ним
+    clean_playlist(client)
+
+    if not os.path.exists('exported.csv'):
+        logging.fatal('Отсутствует файл .csv в директории.')
+        sys.exit()
+
+    list_parsed_track_from_spotify = parse_csv('exported.csv')
+    exported_tracks = []
+
+    if list_parsed_track_from_spotify is not None:
+        for track in list_parsed_track_from_spotify:
+            exported_tracks.append(client.search(
+                text=track,
+                nocorrect=True,
+                type_='track',
+                playlist_in_best=False
+            ))
+
+    for adding_track in exported_tracks:
+        if adding_track.tracks is None:
+            logging.info('Трек не найден')
+            continue
+        track_id = adding_track.tracks.results[0].id
+        album_id = adding_track.tracks.results[0].albums[0].id
+        client.users_playlists_insert_track(
+            kind=edited_playlist.kind,
+            track_id=track_id,
+            album_id=album_id,
+            revision=edited_playlist.revision
+        )
+        logging.info('Трек добавлен')
+        update_playlist_revision()
 
 
-def check_created_playlist(playlists):
+def clean_playlist(client):
+    global edited_playlist
+    client.users_playlists_delete_track(kind=edited_playlist.kind,
+                                        from_=0,
+                                        to=edited_playlist.track_count,
+                                        user_id=client.me.account.uid,
+                                        revision=edited_playlist.revision)
+    if edited_playlist.track_count != 0:
+        update_playlist_revision()
+    logging.info("Плейлист очищен.")
+
+
+def generate_csv_playlist(target_playlist):
+    global track
+    # Создаем CSV-файл для записи данных
+    with open("exported.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Artist", "Track"])  # Записываем заголовки столбцов
+
+        # Перебираем треки в плейлисте и записываем информацию в CSV-файл
+        for track in target_playlist["tracks"]["items"]:
+            artist = track["track"]["artists"][0]["name"]
+            track_name = track["track"]["name"]
+            writer.writerow([artist, track_name])
+
+
+def check_created_playlist(playlists, playlistname):
     global edited_playlist
     for playlist_item in playlists:
-        if playlist_item['title'] == playlist_name:
+        if playlist_item['title'] == playlistname:
             edited_playlist = playlist_item
             return False
         else:
@@ -78,52 +138,5 @@ def parse_csv(filename):
     return data
 
 
-# Проверим наличие нужного плейлиста и создадим при необходимости
-if check_created_playlist(user_playlists):
-    client.users_playlists_create(title=playlist_name, visibility='private', user_id=client.me.account.uid)
-    logging.info("Плейлист создан")
-else:
-    logging.info("Не нужно создавать плейлист")
-
-# Разобраться с difference
-query_for_delete = playlist_difference.add_delete(from_=1, to=2)
-
-# Очистим плейлист, перед работой с ним
-# client.users_playlists_delete_track(kind=edited_playlist.kind,
-#                                     from_=0,
-#                                     to=edited_playlist.track_count,
-#                                     user_id=client.me.account.uid,
-#                                     revision=edited_playlist.revision)
-#
-# update_playlist_revision()
-
-if not os.path.exists('exported.csv'):
-    logging.fatal('Отсутствует файл .csv в директории.')
-    sys.exit()
-
-list_parsed_track_from_spotify = parse_csv('exported.csv')
-exported_tracks = []
-
-if list_parsed_track_from_spotify is not None:
-    for track in list_parsed_track_from_spotify:
-        exported_tracks.append(client.search(
-            text=track,
-            nocorrect=True,
-            type_='track',
-            playlist_in_best=False
-        ))
-
-for adding_track in exported_tracks:
-    if adding_track.tracks is None:
-        logging.info('Трек не найден')
-        continue
-    track_id = adding_track.tracks.results[0].id
-    album_id = adding_track.tracks.results[0].albums[0].id
-    client.users_playlists_insert_track(
-        kind=edited_playlist.kind,
-        track_id=track_id,
-        album_id=album_id,
-        revision=edited_playlist.revision
-    )
-    logging.info('Трек добавлен')
-    update_playlist_revision()
+if __name__ == "__main__":
+    main()
