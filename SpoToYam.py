b38cb829088953b36c359d71ed9312105a166797
Антон Rusty <rusty@deeplay.io>
2023-12-28 11:30:20 +0600

    Метод скачивания плейлиста "Мне нравится" из Spotify

--- SpoToYam.py
+++ SpoToYam.py
@@ -4,14 +4,16 @@ import os.path
 import sys
 
 import spotipy
-from spotipy import SpotifyClientCredentials
+from spotipy import SpotifyClientCredentials, SpotifyOAuth
 from yandex_music import Client
-from yandex_music.utils.difference import Difference
 
 from config_reader import config
 
 # Logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 
 def main():
@@ -24,21 +26,24 @@ def main():
     # Название генерируемого плейлиста
     playlist_name = "Received from Spotify"
 
-    # Класс для создания запросов к плейлисту
-    playlist_difference = Difference()
+    # Синхронизация только лайков
+    only_liked_sync = config.only_liked_sync
 
     # Инициализация плейлиста для работы
     global edited_playlist
+
+    # Синхронизация лайков
+    if only_liked_sync:
+        download_likes_spotify()
+        if os.path.exists('liked_songs.csv'):
+            sync_likes_tracks(client)
+        sys.exit()
 
     # Аутентификация через Spotify API
     auth_manager = SpotifyClientCredentials(
         client_id=config.client_id_spotify.get_secret_value(),
         client_secret=config.client_token_spotify.get_secret_value())
     sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    # Синхронизация лайков
-    if os.path.exists('liked_songs.csv'):
-        sync_likes_tracks(client)
 
     # Получаем плейлист из Spotify
     target_playlist_from_spotify = sp.playlist(config.spotify_playlist_id)
@@ -70,7 +75,7 @@ def add_tracks_to_playlist(client, edited_playlist, exported_tracks, list_parsed
             ))
     for adding_track in exported_tracks:
         if adding_track.tracks is None:
-            logging.info('Трек не найден')
+            logger.info('Трек не найден')
             continue
         track_id = adding_track.tracks.results[0].id
         album_id = adding_track.tracks.results[0].albums[0].id
@@ -80,22 +85,22 @@ def add_tracks_to_playlist(client, edited_playlist, exported_tracks, list_parsed
             album_id=album_id,
             revision=edited_playlist.revision
         )
-        logging.info('Трек добавлен')
+        logger.info('Трек добавлен')
         update_playlist_revision()
 
 
 def check_exported_csv():
     if not os.path.exists('exported.csv'):
-        logging.fatal('Отсутствует файл .csv в директории.')
+        logger.fatal('Отсутствует файл .csv в директории.')
         sys.exit()
 
 
 def check_and_create_playlist(client, playlist_name, user_playlists):
     if check_created_playlist(user_playlists, playlist_name):
         client.users_playlists_create(title=playlist_name, visibility='private', user_id=client.me.account.uid)
-        logging.info("Плейлист создан")
+        logger.info("Плейлист создан")
     else:
-        logging.info("Не нужно создавать плейлист")
+        logger.info("Не нужно создавать плейлист")
 
 
 def clean_playlist(client):
@@ -107,7 +112,7 @@ def clean_playlist(client):
                                         revision=edited_playlist.revision)
     if edited_playlist.track_count != 0:
         update_playlist_revision()
-    logging.info("Плейлист очищен.")
+    logger.info("Плейлист очищен.")
 
 
 def generate_csv_playlist(target_playlist):
@@ -153,6 +158,29 @@ def parse_csv(filename):
     return data
 
 
+def download_likes_spotify():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config.client_id_spotify.get_secret_value(),
+                                                   client_secret=config.client_token_spotify.get_secret_value(),
+                                                   redirect_uri='http://localhost',
+                                                   scope='user-library-read'))
+
+    # Создать CSV файл
+    with open('liked_songs.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Artist", "Track"])
+
+        # Получить первую страницу плейлиста "Мне нравится"
+        results = sp.current_user_saved_tracks()
+        while results:
+            # Записать информацию о треках в файл
+            for item in results['items']:
+                track = item['track']
+                writer.writerow([track['artists'][0]['name'], track['name']])
+
+            # Перейти на следующую страницу
+            results = sp.next(results)
+
+
 def sync_likes_tracks(client):
     list_parsed_track_from_spotify = parse_csv('liked_songs.csv')
     exported_tracks = []
@@ -168,11 +196,11 @@ def sync_likes_tracks(client):
 
         for liked_track in exported_tracks:
             if liked_track.tracks is None:
-                logging.info('Трек не найден')
+                logger.info('Трек не найден')
                 continue
             track_id = liked_track.tracks.results[0].id
             client.users_likes_tracks_add(track_id)
-            logging.info('Трек лайкнут')
+            logger.info('Трек лайкнут')
         os.remove('liked_songs.csv')
 
 
